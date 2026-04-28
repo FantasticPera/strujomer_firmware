ebca9abcd7a7cd394d47299507aac261b6063562
pera <46301880+FantasticPera@users.noreply.github.com>
2026-04-28 17:59:04 +0200

    Dodao u .ino fajl da salje informacije na serial u csv formatu

--- Firmware_Strujomer.ino
+++ Firmware_Strujomer.ino
@@ -1,9 +1,26 @@
+#include "Pins.h"
+#include "ATM90E26_Control.h"
 
-
-void setup(void) {
+void setup() {
   Serial.begin(115200);
+  ATM_init();
+  Serial.println("V,I,F,P,E");
 }
 
 void loop() {
+  float v = ATM_GetVoltage();
+  float i = ATM_GetCurrent();
+  float f = ATM_GetFrequency();
+  float p = ATM_GetActivePower();
+  float e = ATM_GetActiveEnergy();
 
+  // CSV format: V,I,F,P,E
+  Serial.print(v, 2); Serial.print(","); // ovo drugo je broj decimala...
+  Serial.print(i, 3); Serial.print(",");
+  Serial.print(f, 2); Serial.print(",");
+  Serial.print(p, 2); Serial.print(",");
+  Serial.print(e, 6);
+  Serial.println(); // kraj linije
+
+  delay(200); // npr. 5Hz
 }
